db6d7f0f0404c853fa1fd23bc2a6a8815f76a411
Quang Nhat <nhat@codelink.io>
2019-08-14 11:22:05 +0700

    Refine style for login

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import "./App.css";
 import Login from "./Login";
 
-interface Room {
-  id: number;
-  name: string;
-}
-
 const App: React.FC = () => {
   return <Login />;
 };
